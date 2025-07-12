f56a3cb9d9277be8c52c375887fa8c4c739dff94
Louis Liu <105401439+Louis-Liu-one@users.noreply.github.com>
2025-07-12 10:49:58 +0800

    Update minehelper.py

--- minehelper.py
+++ minehelper.py
@@ -11,8 +11,8 @@ from tkinter import ttk
 from tkinter.constants import *
 
 
-class MineHelper(tk.Toplevel):
-    '''扫雷帮助文档。'''
+class Helper(tk.Toplevel):
+    '''帮助文档的父类。子类通过覆盖self.setup_messages()定义。'''
     colors = [  # 预定义的颜色
         None, 'Blue', 'Green',
         'Red', 'DarkBlue', 'DarkRed',
@@ -57,85 +57,6 @@ class MineHelper(tk.Toplevel):
         self.wrong = tk.PhotoImage(file=folder + 'wrong.gif')
         self.click = tk.PhotoImage(file=folder + 'click.gif')
 
-    def setup_messages(self):
-        '''文档内容。'''
-        flag = self.flag      # 省略self.，增强可读性
-        mine = self.mine
-        empty = self.empty
-        opened = self.opened
-
-        tk.Label(
-            self.main_frame, text='Mine Sweeper Helper',
-            font=('Futura', 25, 'bold')).grid(sticky=W)
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_legend(
-            (opened, '4', self.colors[4], 'Opened cell'),
-            (flag, '', None, 'Cell which has been marked as a mine'),
-            (empty, '#', None, 'Cell which can be opened'),
-            (empty, '!', None, 'Cell which must be mine'),).grid(sticky=W)
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_messages(
-            "Don't know how to play this game? Here are some tips:",
-            "First, the number which shows when you click a cell,"
-            " is the number of the mine around the cell you click.")
-        self.grid_cells([
-                [mine, (opened, '3'), mine],
-                [mine, (opened, '4'), (opened, '2')],
-                [(opened, '1'), (opened, '2'), mine]]).grid()
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_messages(
-            "Second, if you know a cell is a mine by logical reasoning,"
-            " you can mark it by clicking.", "Besides, if you know a cell"
-            " is not a mine by logical reasoning,"
-            " you can open it by double clicking.")
-        self.grid_cells([
-                [opened, (opened, '1'), flag],
-                [(opened, '1'), (opened, '2'), (opened, '2')],
-                [(opened, '1'), flag, (opened, '1')]]).grid()
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_messages(
-            "But how to reasoning logically?",
-            "Third, if (8 - n) cells around an n-cell are opened,"
-            " then the other n cells around the n-cell are all mines.")
-        self.grid_cells([
-                [opened, (opened, '1'), '!'],
-                [(opened, '1'), (opened, '2'), (opened, '2')],
-                [(opened, '1'), '!', (opened, '1')]]).grid()
-
-        self.grid_messages(
-            "For example, if 5 cells around a 3-cell are opened,"
-            " then the other 3 cells around the 3-cell are all mines.")
-        self.grid_cells([
-                [(opened, '1'), (opened, '2'), '!'],
-                ['!', (opened, '3'), (opened, '2')],
-                [(opened, '2'), '!', (opened, '1')]]).grid()
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_messages(
-            "Fourth, if n cells around an n-cell have been marked"
-            " as a mine, then all the other (8 - n) cells around the n-cell"
-            " can be opened. For example, n = 3:")
-        self.grid_cells([
-                ['#', '#', flag],
-                [flag, (opened, '3'), '#'],
-                ['#', flag, '#']]).grid()
-
-        self.grid_messages(
-            "This principle is the reverse of the Third Law.")
-        self.grid_cells([
-                ['#', flag, flag],
-                [flag, (opened, '5'), flag],
-                ['#', flag, '#']]).grid()
-        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
-
-        self.grid_messages(
-            "These are the four basic skill to play Mine Sweeper."
-            " You can explore more laws to become a mine sweeper expert!")
-
     def grid_messages(self, *messages):
         '''布置长串信息。可传入多个信息，将分成多个段落。'''
         for message in messages:
@@ -144,7 +65,10 @@ class MineHelper(tk.Toplevel):
                 text=message, width=self.width).grid(sticky=W)
 
     def grid_cells(self, cells):
-        '''布置演示用的扫雷格子，cells是由tuple(图片，文字)，(文字)，或(图片)组成的二维列表。'''
+        '''
+        布置演示用的扫雷格子。
+        cells :: 由tuple(图片，文字)，(文字)，或(图片)组成的二维列表。
+        '''
         frame = tk.Frame(self.main_frame, width=self.small_width)
         for i in range(len(cells)):
             for j in range(len(cells[0])):
@@ -166,10 +90,13 @@ class MineHelper(tk.Toplevel):
         return frame
 
     def grid_legend(self, *legends):
-        '''图例的布置。*legends是形如tuple(图片,图片上的文本,颜色,描述)构成的参数列表。'''
+        '''
+        图例的布置。
+        *legends :: 形如tuple(图片,图片上的文本,颜色,描述)构成的参数列表。
+        '''
         frame = tk.Frame(self.main_frame, width=self.small_width)
         tk.Label(
-            frame, text='Legend',
+            frame, text='Legends',
             font=('Futura', 20)).grid(columnspan=2, sticky=W)
         row = 1
         for image, imagetext, color, text in legends:
@@ -188,13 +115,96 @@ class MineHelper(tk.Toplevel):
         self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
 
 
-class HandleHelper(MineHelper):
+class MineHelper(Helper):
+    '''扫雷帮助文档。'''
+
+    def setup_messages(self):
+        '''文档内容。'''
+        flag = self.flag      # 省略self.，增强可读性
+        mine = self.mine
+        empty = self.empty
+        opened = self.opened
+
+        tk.Label(
+            self.main_frame, text='Mine Sweeper Helper',
+            font=('Futura', 25, 'bold')).grid(sticky=W)
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_legend(
+            (opened, '4', self.colors[4], 'Opened cell'),
+            (flag, '', None, 'Marked cell'),
+            (empty, '#', None, 'Cell which can be opened'),
+            (empty, '!', None, 'Cell which must be a mine'),).grid(sticky=W)
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_messages(
+            "Don't know how to play the game? Here are some tips:",
+            "(1) The showed number when you click a cell,"
+            " is the number of the mine around the cell you click.")
+        self.grid_cells([
+                [mine, (opened, '3'), mine],
+                [mine, (opened, '4'), (opened, '2')],
+                [(opened, '1'), (opened, '2'), mine]]).grid()
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_messages(
+            "(2) If you infer that a cell is a mine,"
+            " you can mark it by clicking.", "Besides, if you"
+            " infer that a cell is not a mine"
+            " you can open it by double clicking.")
+        self.grid_cells([
+                [opened, (opened, '1'), flag],
+                [(opened, '1'), (opened, '2'), (opened, '2')],
+                [(opened, '1'), flag, (opened, '1')]]).grid()
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_messages(
+            "But how to infer?",
+            "(3) If (8 - n) cells around an n-cell are opened,"
+            " then the other n cells around the n-cell are all mines.")
+        self.grid_cells([
+                [opened, (opened, '1'), '!'],
+                [(opened, '1'), (opened, '2'), (opened, '2')],
+                [(opened, '1'), '!', (opened, '1')]]).grid()
+
+        self.grid_messages(
+            "For example, if 5 cells around a 3-cell are opened,"
+            " then the other 3 cells around the 3-cell are all mines.")
+        self.grid_cells([
+                [(opened, '1'), (opened, '2'), '!'],
+                ['!', (opened, '3'), (opened, '2')],
+                [(opened, '2'), '!', (opened, '1')]]).grid()
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_messages(
+            "(4) Similarly, if n cells around an n-cell have been marked"
+            " as a mine, then all the other (8 - n) cells around the n-cell"
+            " can be opened. For example, n = 3:")
+        self.grid_cells([
+                ['#', '#', flag],
+                [flag, (opened, '3'), '#'],
+                ['#', flag, '#']]).grid()
+
+        self.grid_messages(
+            "The (4) Law is the reverse of the (3) Law.")
+        self.grid_cells([
+                ['#', flag, flag],
+                [flag, (opened, '5'), flag],
+                ['#', flag, '#']]).grid()
+        ttk.Separator(self.main_frame, orient=HORIZONTAL).grid(sticky=EW)
+
+        self.grid_messages(
+            "The above are 4 basic laws to play Mine Sweeper."
+            " You can explore more laws to become an expert!")
+
+
+class HandleHelper(Helper):
 
     def setup_messages(self):
         self.grid_messages("First, click to mark a cell as a mine.")
-        self.grid_messages("Second, double click to open a cell.")
-        self.grid_messages("Third, click the green 'click' sign first.")
-        self.grid_messages("Finally, play the game!")
+        self.grid_messages("Second, double-click to open a cell.")
+        self.grid_messages("Third, click the green sign first.")
+        self.grid_messages("Finally, play and have fun!")
 
 
 if __name__ == '__main__':
